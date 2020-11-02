5d3aeacbcefadb1175c40ba1862917de2baace21
Turuntuntun <43621680+Turuntuntun@users.noreply.github.com>
2020-11-02 18:22:14 +0300

    Добавление конфигов

---
+++ configs/routes.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Юра
+ * Date: 18.10.2020
+ * Time: 23:01
+ */
+
+return [
+    'add' => [
+        'controller' => 'Add',
+        'title'      => 'Добавление записи'
+    ],
+    'list' => [
+        'controller' => 'List',
+        'title'      => 'Список записей'
+    ],
+    'update' => [
+        'controller' => 'Update',
+        'title'      => 'Обновление записей'
+    ]
+];
